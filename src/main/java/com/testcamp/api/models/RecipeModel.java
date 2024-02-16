60c6f07862c43412150b95cc5dfcb3b45ffab521
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2024-02-16 15:25:36 -0300

    feat: add entity table recipes

--- src/main/java/com/testcamp/api/models/RecipeModel.java
+++ src/main/java/com/testcamp/api/models/RecipeModel.java
@@ -1,11 +1,27 @@
 package com.testcamp.api.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "recipes")
 public class RecipeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column()
     private String title;
+
+    @Column()
     private String ingredients;
+
+    @Column()
     private String steps;
+
 }
